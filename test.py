029cb642635d27b6046741859bb3a15fb13290f5
park <hosuhosuhosu14@gmail.com>
2025-04-15 17:11:28 +0900

    day17

--- test.py
+++ test.py
@@ -8,6 +8,15 @@
 #         duplicateCnt +=1
 #         if duplicateCnt>1:
 #             continue
-    
-str = "aavasdavsdv"
-print(str[0:3])
+from collections import Counter
+
+input1 = "Hello, I hope you're having a lovely day"
+input1 = list(input1)
+input1 = " ".join(input1).split()
+counter1 = dict(Counter(input1))
+dict1 = dict(sorted(counter1.items(), key=lambda x:x[1]))
+dict1 =dict(filter(lambda elem: elem[1]>3, dict1.items()))
+
+dict1 = list(dict1.items())
+
+print(dict1[0])
